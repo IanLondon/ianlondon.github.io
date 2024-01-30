06a94d2364b6e83339d0e302b6e6b27f9c7a350a
IanLondon <ianlondondu@gmail.com>
2024-01-30 01:36:01 -0500

    fix: style 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
-import { Link, HeadFC, PageProps } from "gatsby";
+import { Link, PageProps } from "gatsby";
+import Page from "../components/Page";
+
+export { Head } from "../components/Head";
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main>
+    <main className="text-center text-xl pt-16 pb-4">
       <h1>Page not found.</h1>
-      <p>That's a 404, bb</p>
-      <p>😔</p>
-      <p>
-        <Link to="/">Go home</Link>.
+      <p className="p-4 text-3xl">😔</p>
+      <p className="text-amber-500 text-3xl underline">
+        <Link to="/">Back to home</Link>.
       </p>
     </main>
   );
 };
 
 export default NotFoundPage;
-
-export const Head: HeadFC = () => <title>Not found</title>;
